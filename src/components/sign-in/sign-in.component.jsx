18596e095311cfe02d01fc71140aae75a4468b31
Juan Tobias <juantobiasdl7@gmail.com>
2020-04-22 13:44:45 -0500

    finishing sign in handleSubmit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -14,19 +14,26 @@ class SignIn extends React.Component{
         this.state = {
             email:'',
             password:''
-        }
+        };
     }
 
-    handleSumit = event =>{
+    handleSumit = async event =>{
         event.preventDefault();
 
-        this.setState({email: '', password:'' })
-    }
+        const { email, password } = this.state;
+
+        try{
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email: '', password:'' });
+        }catch(error){
+            console.log(error);
+        }
+    };
 
     handleChange = event => {
         const { value, name } = event.target;
 
-        this.setState({ [name]: value})
+        this.setState({ [name]: value});
     }
 
     render(){
@@ -35,7 +42,7 @@ class SignIn extends React.Component{
                 <h2>I already have an account</h2>
                 <span>Sign in with yout email and password</span>
 
-                <form onSumit={this.handleSumit}>
+                <form onSubmit ={this.handleSumit}>
                     <FormInput 
                         name="email" 
                         type="email" 
